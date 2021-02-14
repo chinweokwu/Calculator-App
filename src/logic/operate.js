21876526567d283baa1deb2e3b99f88f26cb0d18
chinweokwu <paulcmorah@gmail.com>
2021-02-15 00:10:19 +0100

    Implement the operate.js module

---
+++ src/logic/operate.js
@@ -0,0 +1,32 @@
+import Big from 'big.js';
+
+const Operate = (numberOne, numberTwo, operation) => {
+  let result = 0;
+  const i = Big(numberOne);
+  const j = Big(numberTwo);
+
+  switch (operation) {
+    case '+':
+      result = i + j;
+      break;
+
+    case '-':
+      result = i - j;
+      break;
+
+    case '*':
+      result = i * j;
+      break;
+
+    case '/':
+      result = i / j;
+      break;
+
+    default:
+      result /= 100;
+      break;
+  }
+  return result;
+};
+
+export default Operate;
