805ab5836c32422964bdb3e4d75a6f65f0330802
chinweokwu <paulcmorah@gmail.com>
2021-02-14 03:12:07 +0100

    created display

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Display from './display';
+// import ButtonPanel from './buttonpanel';
+
+const App = () => (
+  <>
+    <Display />
+    {/* <ButtonPanel /> */}
+  </>
+);
+
+export default App;
---
+++ src/components/display.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+import PropTypes from 'prop-types';
+
+const Display = props => {
+  const { results } = props;
+
+  return (<input type="text" value={results} />);
+};
+
+Display.defaultProps = {
+  results: '0',
+};
+
+Display.propTypes = {
+  results: PropTypes.string,
+};
+
+export default Display;
