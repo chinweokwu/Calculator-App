b81bdedc14753b2654e46cf20be69d86d2cc572a
chinweokwu <paulcmorah@gmail.com>
2021-02-15 00:36:57 +0100

    Add Calculate.js to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Display from './display';
 import ButtonPanel from './buttonpanel';
-// // eslint-disable-next-line no-unused-vars
-// import Calculate from '../logic/calculate';
+// eslint-disable-next-line no-unused-vars
+import Calculate from '../logic/calculate';
 
 const App = () => (
   <>
