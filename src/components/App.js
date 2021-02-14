869f917a6fc9070466e5419ab486ffed00c1549e
chinweokwu <paulcmorah@gmail.com>
2021-02-14 17:40:28 +0100

    App renders Display & ButtonPanel component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import Display from './display';
+import ButtonPanel from './buttonpanel';
+
 function App() {
   return (
-    <div>
-      <h1>Hello React Calculator: Milestone One</h1>
-    </div>
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
   );
 }
 
