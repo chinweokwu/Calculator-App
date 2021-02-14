16b3ce9e7b8674df59791510cc5b248cafb88dda
chinweokwu <paulcmorah@gmail.com>
2021-02-14 03:28:12 +0100

    added button group

---
+++ src/components/buttonpanel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from './button';
+import {Group1, Group2, Group3, Group4, Group5,} from '../buttonGroup';
+
+const ButtonPanel = () => {
+
+};
+
+export default ButtonPanel;
