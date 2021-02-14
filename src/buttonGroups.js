c9eadde63ca79895e93ed669f2d9974b7dd849a3
chinweokwu <paulcmorah@gmail.com>
2021-02-14 18:36:18 +0100

    create button group component

---
+++ src/buttonGroups.js
@@ -0,0 +1,75 @@
+const GroupOne = [
+  {
+    name: 'AC',
+  },
+  {
+    name: '+/-',
+  },
+  {
+    name: '%',
+  },
+  {
+    name: '+',
+  },
+];
+
+const GroupTwo = [
+  {
+    name: '7',
+  },
+  {
+    name: '8',
+  },
+  {
+    name: '9',
+  },
+  {
+    name: 'X',
+  },
+];
+
+const GroupThree = [
+  {
+    name: '4',
+  },
+  {
+    name: '5',
+  },
+  {
+    name: '6',
+  },
+  {
+    name: '-',
+  },
+];
+
+const GroupFour = [
+  {
+    name: '1',
+  },
+  {
+    name: '2',
+  },
+  {
+    name: '3',
+  },
+  {
+    name: '+',
+  },
+];
+
+const GroupFive = [
+  {
+    name: '0',
+  },
+  {
+    name: '.',
+  },
+  {
+    name: '=',
+  },
+];
+
+export default {
+  GroupOne, GroupTwo, GroupThree, GroupFour, GroupFive,
+};
