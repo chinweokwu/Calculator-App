c91730e64d2941e0e5fffaddba6c849c2d245487
chinweokwu <paulcmorah@gmail.com>
2021-02-22 14:59:41 +0100

    add style to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,11 +4,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => (
   <nav>
     <div>
-      <Link to="/">Home</Link>
-      {' | '}
-      <Link to="/calculator">Calculator</Link>
-      {' | '}
-      <Link to="/quote">Quote</Link>
+      <Link className="navitem" to="/">Home</Link>
+      <Link className="navitem" to="/calculator">Calculator</Link>
+      <Link className="navitem" to="/quote">Quote</Link>
     </div>
   </nav>
 );
