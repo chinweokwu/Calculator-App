8172b6000564ae474382d50c394cefe56ab66189
chinweokwu <paulcmorah@gmail.com>
2021-02-26 02:16:05 +0100

    add error message in quotes

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -4,15 +4,24 @@ import axios from 'axios';
 const Quotes = () => {
   const [quotes, setQuotes] = useState('');
   const [author, setAuthor] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   const handleClick = () => {
     axios.get('https://type.fit/api/quotes')
       .then(res => {
         const randomQuotes = Math.floor(Math.random() * res.data.length);
+        setLoading(false);
         setQuotes(res.data[randomQuotes].text);
         setAuthor(res.data[randomQuotes].author);
+        setError('');
       })
-      .catch(err => err);
+      .catch(() => {
+        setLoading(false);
+        setQuotes('');
+        setAuthor('');
+        setError('Something went wrong bro!');
+      });
   };
 
   useEffect(() => {
@@ -22,7 +31,8 @@ const Quotes = () => {
   return (
     <div className="quote">
       <div className="login-box">
-        <h3>{quotes}</h3>
+        <h3>{loading ? 'loading...' : quotes}</h3>
+        <h3>{error || null}</h3>
         <div className="author">{author}</div>
         <button type="button" onClick={handleClick}>
           <span />
