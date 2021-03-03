90a41038f891395ea9fc849e575226e42b59ba7b
Morah Paul <paulcmorah@gmail.com>
2021-03-03 01:55:45 +0100

    fix broken code

--- src/logic/calculator.js
+++ src/logic/calculator.js
@@ -1,42 +1,42 @@
 import Operator from './operate';
 
-const Calculate = (data, buttonName) => {
-  const { total, next, operation } = data;
-  let result;
+const Calculate = (value, btnName) => {
+  const { total, next, operation } = value;
+  let res;
 
-  if (buttonName === 'AC' || total === 'Error') {
-    result = { total: null, next: null, operation: null };
-  } else if (buttonName === '+/-') {
+  if (btnName === 'AC' || total === 'Error') {
+    res = { total: null, next: null, operation: null };
+  } else if (btnName === '+/-') {
     if (next === null) {
-      result = { total: total ? (total * -1).toString() : total };
+      res = { total: total ? (total * -1).toString() : total };
     } else {
-      result = { next: (next * -1).toString() };
+      res = { next: (next * -1).toString() };
     }
-  } else if (buttonName === '%') {
+  } else if (btnName === '%') {
     if (total === null && next !== null) {
-      result = { next: next ? (next / 100).toString() : next };
+      res = { next: next ? (next / 100).toString() : next };
     } else if (next === null && total !== null) {
-      result = { total: total ? (total / 100).toString() : total };
+      res = { total: total ? (total / 100).toString() : total };
     }
-  } else if (['+', '-', 'X', '/'].includes(buttonName)) {
-    result = {
+  } else if (['+', '-', 'X', '/'].includes(btnName)) {
+    res = {
       total: total ?? next,
       next: null,
-      operation: buttonName,
+      operation: btnName,
     };
-  } else if (buttonName === '=') {
-    result = {
+  } else if (btnName === '=') {
+   res res = {
       total: Operator(total, next, operation).toString(),
       next: null,
       operation: null,
     };
-  } else if (buttonName === '.' && next) {
-    result = { next: next.includes('.') ? next : `${next}.` };
+  } else if (btnName === '.' && next) {
+    res = { next: next.includes('.') ? next : `${next}.` };
   } else {
-    result = { next: next ? next + buttonName : buttonName };
+    res = { next: next ? next + btnName : btnName };
   }
 
-  return result;
+  return res;
 };
 
 export default Calculate;
